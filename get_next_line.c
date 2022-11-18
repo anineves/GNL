d145e29cf33a08b531f0918a1b79f5d8e290a00f
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2022-11-18 17:23:59 +0000

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -7,49 +7,40 @@ char		*ft_remove(char *backup)
 	char	*line;
 
 	i = 0;
-	// find len of first line
 	while (backup[i] && backup[i] != '\n')
 		i++;
-	// if eol == \0 return NULL
 	if (!backup[i])
 	{
 		free(backup);
 		return (NULL);
 	}
-	// len of file - len of firstline + 1
 	line = ft_calloc((ft_strlen(backup) - i + 1), sizeof(char));
 	i++;
 	j = 0;
-	// line == buffer
 	while (backup[i])
 		line[j++] = backup[i++];
 	free(backup);
 	return (line);
 }
 
-// take line for return
+// bBuscar linha parea returnar
 char	*ft_get_line(char *backup)
 {
 	char	*line;
 	int		size;
 
 	size = 0;
-	// if no line return NULL
 	if (!*backup)
 		return (NULL);
-	// go to the eol
 	while (backup[size] && backup[size] != '\n')
 		size++;
-	// malloc to eol
 	line = ft_calloc(size + 2, sizeof(char));
 	size = 0;
-	// line = buffer
 	while (backup[size] && backup[size] != '\n')
 	{
 		line[size] = backup[size];
 		size++;
 	}
-	// if eol is \0 or \n, replace eol by \n
 	if (backup[size] == '\n')
 		line[size++] = '\n';
 	return (line);
@@ -60,26 +51,20 @@ char	*ft_read_file(int fd, char *backup)
 	char	*buffer;
 	int		byte_read;
 
-	// malloc if res dont exist
 	if (!backup)
 		backup= ft_calloc(1, 1);
-	// malloc buffer
 	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	byte_read = 1;
 	while (byte_read > 0 && !ft_strchr(backup, '\n'))
 	{
-		// while not eof read
 		byte_read = read(fd, buffer, BUFFER_SIZE);
 		if (byte_read < 0)
 		{
 			free(buffer);
 			return (NULL);
 		}
-		// 0 to end for leak
 		buffer[byte_read] = '\0';
-		// join and free
 		backup= ft_strjoin(backup, buffer);
-		// quit if \n find
 	
 	}
 	free(buffer);
@@ -94,7 +79,6 @@ char	*get_next_line(int fd)
 	static char	*backup;
 	char		*line;
 
-	// error handling
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	backup = ft_read_file(fd, backup);
@@ -107,10 +91,10 @@ char	*get_next_line(int fd)
 
 int main()
 {
-	int fd = open("a.txt", O_RDONLY);
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
+	int fd = open("teste.txt", O_RDONLY);
+	printf("linha1 %s", get_next_line(fd));
+	printf("linha2 %s", get_next_line(fd));
+	printf("linha3 %s", get_next_line(fd));
+	printf("linha4 %s", get_next_line(fd));
 	return 0;
 }
