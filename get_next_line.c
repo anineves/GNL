6885f3adc6d32e372dc5b6325982cb7e0e89764d
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2022-11-19 11:46:38 +0000

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -1,14 +1,27 @@
-#include "get_next_line.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asousa-n <asousa-n@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 15:30:28 by asousa-n          #+#    #+#             */
+/*   Updated: 2022/11/14 15:30:31 by asousa-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "get_next_line.h"
 //remove a linha lida. Ela recebe como parâmetro a static , 
 //nela eu procuro até a quebra de linha \n ou até chegar no final,
 //se chega a EOF, dou um free na variável e retorno NULL , senão, 
-//eu malloco uma nova variavel com o (tamanho total de backup, menos o tamanho da linha),
+//eu malloco uma nova variavel com o (tamanho total de backup, 
+//menos o tamanho da linha),
 //copio o restante de backup pra uma nova variável e dou free nela.
-char		*ft_remove(char *backup)
+
+char	*ft_remove(char *backup)
 {
-	int		i;
-	int		j;
+	size_t	i;
+	size_t	j;
 	char	*line;
 
 	i = 0;
@@ -24,70 +37,77 @@ char		*ft_remove(char *backup)
 	j = 0;
 	while (backup[i])
 		line[j++] = backup[i++];
+	line[j] = '\0';
 	free(backup);
 	return (line);
 }
 
 // bBuscar linha parea returnar
-// Como parâmetro ainda vamos usar o backup, faço uma verificação se a backup 
-//não está com o primeiro caracter como null byte ('\0')
+// Como parâmetro ainda vamos usar o backup, faço uma verificação 
+//se a backup não está com o primeiro caracter como null byte ('\0')
 //percorro ela inteira até achar uma ('\n') ou o final dela, 
-//crio com o malloc uma string com o tamanho que li + 2 espaços, pra caso haja um \n ,
+//crio com o malloc uma string com o tamanho que li + 2 espaços,
+// pra caso haja um \n ,
 //eu coloque ele e logo em seguida o (\0) e retorno essa linha.
 char	*ft_get_line(char *backup)
 {
 	char	*line;
-	int		size;
+	size_t	size;
+	size_t	i;
 
 	size = 0;
-	if (!*backup)
+	i = 0;
+	if (!backup[i])
 		return (NULL);
 	while (backup[size] && backup[size] != '\n')
 		size++;
 	line = ft_calloc(size + 2, sizeof(char));
-	size = 0;
-	while (backup[size] && backup[size] != '\n')
+	while (i <= size)
 	{
-		line[size] = backup[size];
-		size++;
+		line[i] = backup[i];
+		i++;
 	}
-	if (backup[size] == '\n')
-		line[size++] = '\n';
+	line[i] = '\0';
 	return (line);
 }
 //Ler arquivo
 //recebe como parâmetro o fd do arquivo e a variavel estática.
 //crio um buffer com o tamnaho do BUFFER_SIZE e uso a função read() 
-//pra ler o arquivo até encontrar o final dele - que seria retornado 0 - ou até encontrar uma linha (\n)
-//caso ocorra algum erro, o laço de repetição é parado pois vai ser retornado -1,
+//pra ler o arquivo até encontrar o final dele - que seria retornado 
+//0 - ou até encontrar uma linha (\n)
+//caso ocorra algum erro, o laço de repetição é parado pois vai ser
+// retornado -1,
 //free a variavel buffer que tinha mallocado e retorno NULL,
-//caso contrário, uso a função ft_strjoin()(onde adicionei free) para juntar o conteúdo de backup com o buffer.
+//caso contrário, uso a função ft_strjoin()(onde adicionei free) 
+//para juntar o conteúdo de backup com o buffer.
+
 char	*ft_read_file(int fd, char *backup)
 {
 	char	*buffer;
 	int		byte_read;
 
 	if (!backup)
-		backup= ft_calloc(1, 1);
+		backup = ft_calloc(1, sizeof(char));
 	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-	byte_read = 1; //inicializo em 1 para entrar no loop pela primeira ve
-	while (byte_read > 0 && !ft_strchr(backup, '\n'))
+	byte_read = 1;
+//inicializo em 1 para entrar no loop pela primeira ve
+	while (byte_read > 0)
 	{
 		byte_read = read(fd, buffer, BUFFER_SIZE);
-		if (byte_read < -1) // porque se read der erro devolve 0
+		if (byte_read == -1 || (byte_read == 0 && backup == NULL))
+// porque se read der erro devolve 0
 		{
 			free(buffer);
+			free(backup);
 			return (NULL);
 		}
 		buffer[byte_read] = '\0';
-		backup= ft_strjoin(backup, buffer);
-	
+		backup = ft_strjoin(backup, buffer);
+		if (ft_strchr(backup, '\n'))
+			break ;
 	}
 	free(buffer);
-	if (byte_read < 0)
-		return (NULL);
-	else
-		return (backup);
+	return (backup);
 }
 
 char	*get_next_line(int fd)
@@ -97,14 +117,15 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	backup = ft_read_file(fd, backup); //chamo a funcao que vai ler do buffer e guardo a nova backup.
+	backup = ft_read_file(fd, backup);
+	//chamo a funcao que vai ler do buffer e guardo a nova backup.
 	if (!backup)
 		return (NULL);
 	line = ft_get_line(backup); // a linha a ser usada
 	backup = ft_remove(backup); //remove a linha lida.
 	return (line);
 }
-
+/*
 int main()
 {
 	int fd = open("teste.txt", O_RDONLY);
@@ -113,4 +134,4 @@ int main()
 	printf("linha3 %s", get_next_line(fd));
 	printf("linha4 %s", get_next_line(fd));
 	return 0;
-}
+} */
