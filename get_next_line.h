caded8c2ddf40b7ed50af27d7179feab0067f25d
Ana Isabel Neves <85195243+anineves@users.noreply.github.com>
2022-11-19 11:47:09 +0000

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -1,9 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asousa-n <asousa-n@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/14 15:30:49 by asousa-n          #+#    #+#             */
+/*   Updated: 2022/11/14 15:31:40 by asousa-n         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 42
-#endif 
 
 # include <stdlib.h>
 # include <sys/types.h>
@@ -14,15 +22,17 @@
 # include <fcntl.h>
 # include <string.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif 
 
 char	*get_next_line(int fd);
 char	*ft_read_file(int fd, char *backup);
 char	*ft_line(char *backup);
 char	*ft_remove(char *backup);
 char	*ft_strjoin(char *s, char *buffer);
-char    *ft_strchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
 void	*ft_calloc(size_t nmemb, size_t size);
-
 size_t	ft_strlen(const char *str);
 
 #endif
